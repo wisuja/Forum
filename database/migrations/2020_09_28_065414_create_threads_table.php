e214e99224927a404e82bbe1f889c2957b10388d
wisuja <williamsj26.wsj@gmail.com>
2020-09-28 14:03:43 +0700

    Changed body type to text

--- database/migrations/2020_09_28_065414_create_threads_table.php
+++ database/migrations/2020_09_28_065414_create_threads_table.php
@@ -17,7 +17,7 @@ class CreateThreadsTable extends Migration
             $table->id();
             $table->integer('user_id');
             $table->string('title');
-            $table->string('body');
+            $table->text('body');
             $table->timestamps();
         });
     }
