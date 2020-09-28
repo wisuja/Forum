fd7b745b6e22866fcd5937ff0b00114b03717ce9
wisuja <williamsj26.wsj@gmail.com>
2020-09-28 14:46:42 +0700

    Scaffolding Laravel/UI Bootstrap Auth

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/threads', [ThreadsController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
