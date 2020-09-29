44b719e29fc6c299a32af290d6bdaa76fd763456
wisuja <williamsj26.wsj@gmail.com>
2020-09-29 09:28:14 +0700

    Created unit test for Reply

---
+++ tests/Unit/ReplyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Reply;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ReplyTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_it_has_an_owner()
+    {
+        $reply = Reply::factory()->create();
+        $this->assertInstanceOf(User::class, $reply->owner);
+    }
+}
