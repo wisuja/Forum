d83d5054514f48ed6bd4f7c551a9324f0be0945a
wisuja <williamsj26.wsj@gmail.com>
2020-09-29 09:39:02 +0700

    Added new relationship on Thread model with User model

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -18,4 +18,9 @@ class Thread extends Model
     {
         return $this->hasMany(Reply::class);
     }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
