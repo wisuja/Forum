2e23c655f56fe743a3c4957e0534eea945892042
wisuja <williamsj26.wsj@gmail.com>
2020-09-29 11:23:11 +0700

    Added form to add reply or message to tell a user to sign in if not signed in yet.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -21,12 +21,31 @@
             </div>
         </div>
     </div>
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mb-3">
         <div class="col-md-8">
             @foreach ($thread->replies as $reply)
                 @include('threads.reply')
             @endforeach
         </div>
     </div>
+    @if (auth()->check())
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form action="{{ $thread->path() }}/replies" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <textarea name="body" id="body" class="form-control" rows="5" placeholder="Have something to say?"></textarea>    
+                    </div>
+                    <button type="submit" class="btn btn-primary">Post</button>
+                </form>
+            </div>
+        </div>
+    @else
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center">
+                <p>You need to <a href="{{ route('login') }}">sign in</a> to participate in the thread.</p>
+            </div>
+        </div>
+    @endif
 </div>
 @endsection
