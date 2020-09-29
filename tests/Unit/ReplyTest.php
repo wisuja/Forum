f13f07c63073145cce7dfcfd4b888340c1a6b955
wisuja <williamsj26.wsj@gmail.com>
2020-09-29 09:32:36 +0700

    Added PHPDoc for test_it_has_an_owner in ReplyTest

--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -11,6 +11,11 @@ class ReplyTest extends TestCase
 {
     use DatabaseMigrations;
 
+    /**
+     * test_it_has_an_owner
+     *
+     * @return void
+     */
     public function test_it_has_an_owner()
     {
         $reply = Reply::factory()->create();
