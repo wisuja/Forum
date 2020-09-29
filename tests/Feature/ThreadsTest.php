5b2f38de159bc58c3426a9628d1d6223347de556
wisuja <williamsj26.wsj@gmail.com>
2020-09-29 08:41:31 +0700

    Added test case for viewing replies on associated thread

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -43,4 +43,17 @@ class ThreadsTest extends TestCase
         $this->get($this->thread->path())
             ->assertSee($this->thread->id);
     }
+
+    /**
+     * test_a_user_can_read_replies_that_are_associated_with_a_thread
+     *
+     * @return void
+     */
+    public function test_a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = Reply::factory()->create(['thread_id' => $this->thread->id]);
+
+        $this->get($this->thread->path())
+            ->assertSee($reply->body);
+    }
 }
