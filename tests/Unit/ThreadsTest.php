8a48589d5216087a206f387dfe8f953f3b44c890
wisuja <williamsj26.wsj@gmail.com>
2020-09-29 09:38:41 +0700

    Created new unit test for ThreadsTest checking to see does a thread has an owner

--- tests/Unit/ThreadsTest.php
+++ tests/Unit/ThreadsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Models\Thread;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
@@ -10,7 +11,7 @@ use Tests\TestCase;
 class ThreadsTest extends TestCase
 {
     use DatabaseMigrations;
-    
+
     /**
      * test_a_thread_has_replies
      *
@@ -20,5 +21,16 @@ class ThreadsTest extends TestCase
     {
         $thread = Thread::factory()->create();
         $this->assertInstanceOf(Collection::class, $thread->replies);
+    }
+
+    /**
+     * test_a_thread_has_a_creator
+     *
+     * @return void
+     */
+    public function test_a_thread_has_a_creator()
+    {
+        $thread = Thread::factory()->create();
+        $this->assertInstanceOf(User::class, $thread->creator);
     }
 }
