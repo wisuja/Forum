a330a9cae92c1fc2b83d0a256414e2eb7fccd743
wisuja <williamsj26.wsj@gmail.com>
2020-09-29 15:20:43 +0700

    Adding a new test for checking if a user can filter threads according to a channel

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Channel;
 use App\Models\Reply;
 use App\Models\Thread;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -55,5 +56,22 @@ class ReadThreadsTest extends TestCase
 
         $this->get($this->thread->path())
             ->assertSee($reply->body);
+    }
+
+    /**
+     * test_a_user_can_filter_threads_according_to_a_channel
+     *
+     * @return void
+     */
+    public function test_a_user_can_filter_threads_according_to_a_channel()
+    {
+        $channel = create(Channel::class);
+        $threadInChannel = create(Thread::class, ['channel_id' => $channel->id]);
+        $threadNotInChannel = create(Thread::class);
+
+        $this->withoutExceptionHandling()
+            ->get("/threads/{$channel->slug}")
+            ->assertSee($threadInChannel->title)
+            ->assertDontSee($threadNotInChannel->title);
     }
 }
