1f7c7764b2ee15cbcffa15d9e5d79d5b401e15e3
wisuja <williamsj26.wsj@gmail.com>
2020-09-29 09:27:14 +0700

    Added card header for replier's info

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -23,11 +23,14 @@
         <div class="col-md-8">
             @foreach ($thread->replies as $reply)
             <div class="card mt-3">
-                <div class="card-header"></div>
-                    <div class="card-body">
-                        {{ $reply->body }}
-                    </div>
+                <div class="card-header">
+                    <a href="#">{{ $reply->owner->name }}</a> said
+                    {{ $reply->created_at->diffForHumans() }}
                 </div>
+                <div class="card-body">
+                    {{ $reply->body }}
+                </div>
+            </div>
             @endforeach
         </div>
     </div>
