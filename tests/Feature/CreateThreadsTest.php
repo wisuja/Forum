ca75c9d49224ee88ee5b00a0864d19879a2c3b6e
wisuja <williamsj26.wsj@gmail.com>
2020-09-29 14:47:50 +0700

    Creating a test case for validate request given when creating a POST to /threads

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Channel;
 use App\Models\Thread;
 use App\Models\User;
 use Illuminate\Auth\AuthenticationException;
@@ -44,5 +45,47 @@ class CreateThreadsTest extends TestCase
 
         $this->post('/threads')
             ->assertRedirect(route('login'));
+    }
+
+    /**
+     * test_a_thread_require_a_title
+     *
+     * @return void
+     */
+    public function test_a_thread_require_a_title()
+    {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+    }
+
+    /**
+     * test_a_thread_require_a_body
+     *
+     * @return void
+     */
+    public function test_a_thread_require_a_body()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    public function test_a_thread_require_a_valid_channel_id()
+    {
+        Channel::factory()->count(2)->create();
+
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
+    function publishThread($override)
+    {
+        $this->signIn();
+
+        $thread = make(Thread::class, $override);
+
+        return $this->post('/threads', $thread->toArray());
     }
 }
