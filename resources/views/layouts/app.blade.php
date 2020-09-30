258540ee956061dc7993dd07ea0c4beaa4a37e9b
wisuja <williamsj26.wsj@gmail.com>
2020-09-30 14:16:02 +0700

    Adding style tag in app.blade.php and add link to popular threads in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,21 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <style>
+        body {
+            padding-bottom: 100px;
+        }
+
+        .level {
+            display: flex;
+            align-items: center;
+        }
+
+        .flex {
+            flex: 1
+        }
+    </style>
 </head>
 <body style="padding-bottom: 100px">
     <div id="app">
@@ -42,6 +57,7 @@
                                 @if (auth()->check())
                                     <a class="dropdown-item" href="/threads?by={{ auth()->user()->name }}">My threads</a>
                                 @endif
+                                <a class="dropdown-item" href="/threads?popular=1">Popular</a>
                             </div>
                         </li>
                         <li class="nav-item">
