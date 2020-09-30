e75a75fbc4bdc70dcebb414887df878775db2e8f
wisuja <williamsj26.wsj@gmail.com>
2020-09-30 09:52:42 +0700

    Refactoring by using `channels` value given

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -22,7 +22,7 @@
                         <label for="channel_id">Choose a channel:</label>
                         <select name="channel_id" id="channel_id" class="form-control" required>
                           <option value="">Choose one...</option>
-                          @foreach (App\Models\Channel::all() as $channel)
+                          @foreach ($channels as $channel)
                             <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected': '' }}>
                               {{ $channel->name }}
                             </option>
