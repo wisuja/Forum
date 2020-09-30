de2cd96cbc99b63549e39c4cf3b14fe8bd73907a
wisuja <williamsj26.wsj@gmail.com>
2020-09-30 14:15:44 +0700

    Refactoring index view

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,23 +4,35 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-          <h3>Forum Threads</h3>
-          @foreach ($threads as $thread)
-            <div class="card mt-3">
-                <div class="card-header"><a href="{{ $thread->path() }}">{{ $thread->title }}</a></div>
+					<div class="card mt-3">
+						<div class="card-header">
+							Forum Threads
+						</div>
+						<div class="card-body">
+							@if (session('status'))
+								<div class="alert alert-success" role="alert">
+									{{ session('status') }}
+								</div>
+							@endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+         			@foreach ($threads as $thread)
+                <article>
+									<div class="level">
+										<h4 class="flex">
+											<a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+										</h4>
 
-                    {{ $thread->body }}
-                </div>
-            </div>
-          @endforeach
-        </div>
+										<a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}</a>
+									</div>
+									<div class="body">
+										{{ $thread->body }}
+									</div>
+								</article>
+								<hr>
+								@endforeach
+						</div>
+					</div>
+      </div>
     </div>
 </div>
 @endsection
