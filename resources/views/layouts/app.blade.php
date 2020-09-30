eced3df970669bd21e5b954cac27489ea38af666
wisuja <williamsj26.wsj@gmail.com>
2020-09-30 10:31:43 +0700

    Updating links in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,8 +33,16 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/threads">Threads</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Browse
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="/threads">All threads</a>
+                                @if (auth()->check())
+                                    <a class="dropdown-item" href="/threads?by={{ auth()->user()->name }}">My threads</a>
+                                @endif
+                            </div>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/threads/create">New Thread</a>
@@ -48,7 +56,7 @@
                                     <a class="dropdown-item" href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a>
                                 @endforeach
                             </div>
-                            </li>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
