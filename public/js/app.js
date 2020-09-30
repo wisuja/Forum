f47cb1d4f01a5515ec5f58905b9a75e875cfb733
wisuja <williamsj26.wsj@gmail.com>
2020-09-30 13:16:01 +0700

    Building webpack

--- public/js/app.js
+++ public/js/app.js
@@ -37331,8 +37331,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Projects\Websites\forum8\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Projects\Websites\forum8\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Projects\Websites\forum\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Projects\Websites\forum\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
