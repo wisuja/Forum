30d90e80dd401c47aea492e4cda20d2512096577
wisuja <williamsj26.wsj@gmail.com>
2020-09-30 14:16:16 +0700

    Creating a new filter for popular threads

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -6,12 +6,17 @@ use App\Models\User;
 
 class ThreadFilters extends Filters
 {
-  protected $filters = ['by'];
+  protected $filters = ['by', 'popular'];
 
   public function by($username)
   {
     $user = User::where('name', $username)->firstOrFail();
 
     return $this->builder->where('user_id', $user->id);
+  }
+
+  public function popular()
+  {
+    return $this->builder->reorder('replies_count', 'desc');
   }
 }
