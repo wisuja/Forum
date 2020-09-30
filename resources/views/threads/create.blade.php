e01d0711d42e8daa916f9c19beb7664e685df258
wisuja <williamsj26.wsj@gmail.com>
2020-09-30 09:39:29 +0700

    Adding combobox for channel_id in create view

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -19,15 +19,39 @@
                     <form action="/threads" method="POST">
                       @csrf
                       <div class="form-group">
+                        <label for="channel_id">Choose a channel:</label>
+                        <select name="channel_id" id="channel_id" class="form-control" required>
+                          <option value="">Choose one...</option>
+                          @foreach (App\Models\Channel::all() as $channel)
+                            <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected': '' }}>
+                              {{ $channel->name }}
+                            </option>
+                          @endforeach
+                        </select>
+                      </div>
+                      
+                      <div class="form-group">
                         <label for="title">Title:</label>
-                        <input type="text" name="title" id="title" class="form-control">
+                        <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}" required>
                       </div>
                       <div class="form-group">
                         <label for="body">Body:</label>
-                        <textarea name="body" id="body" class="form-control" rows="5"></textarea>
+                        <textarea name="body" id="body" class="form-control" rows="5" value="{{ old('body') }}" required></textarea>
                       </div>
-                      <button type="submit" class="btn btn-primary">Publish</button>
+                      <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Publish</button>
+                      </div>
+
+                      @if (count($errors))
+                          <ul class="alert alert-danger">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                          </ul>
+                      @endif
+
                     </form>
+
                 </div>
             </div>
         </div>
