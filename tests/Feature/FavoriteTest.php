b3bb1423fe7d703c7de9043b9e1fad6564b94a9a
wisuja <williamsj26.wsj@gmail.com>
2020-09-30 15:17:25 +0700

    Creating a test case for favoriting a reply

---
+++ tests/Feature/FavoriteTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Reply;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FavoriteTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_an_authenticated_user_can_favorite_any_reply()
+    {
+        $this->signIn();
+
+        $reply = create(Reply::class);
+
+        $this->withoutExceptionHandling()
+            ->post("/replies/{$reply->id}/favorites");
+
+        $this->assertCount(1, $reply->favorites);
+    }
+
+    public function test_guests_can_not_favorite_any_reply()
+    {
+        $this->withExceptionHandling()
+            ->post("/replies/1/favorites")
+            ->assertRedirect(route('login'));
+    }
+
+    public function test_an_authenticated_user_can_only_favorite_a_reply_once()
+    {
+        $this->signIn();
+
+        $reply = create(Reply::class);
+
+        $this->withoutExceptionHandling()
+            ->post("/replies/{$reply->id}/favorites");
+        $this->withoutExceptionHandling()
+            ->post("/replies/{$reply->id}/favorites");
+
+        $this->assertCount(1, $reply->favorites);
+    }
+}
