56d1f3752ffa9222e30cc78d5a42cae941f4bf36
wisuja <williamsj26.wsj@gmail.com>
2020-10-01 09:44:18 +0700

    Adding cache for channels to reduce amout of queries to db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Channel;
 use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -29,7 +30,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         View::composer('*', function ($view) {
-            $view->with('channels', Channel::all());
+            $channels = Cache::rememberForever('channels', function () {
+                return Channel::all();
+            });
+            $view->with('channels', $channels);
         });
     }
 }
