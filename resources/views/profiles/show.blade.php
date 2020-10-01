36e003bbef6792ef31ae73ed79f52eb430ca4a55
wisuja <williamsj26.wsj@gmail.com>
2020-10-01 14:05:21 +0700

    Refactoring profiles.show view for readability

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -6,30 +6,20 @@
       <div class="col-md-12">
         <div class="page-header">
           <h1>{{ $profileUser->name }}</h1>
-          <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
+          <small>Joined since {{ $profileUser->created_at->diffForHumans() }}</small>
         </div>
       </div>
     </div>
     <div class="row">
       <div class="col-md-12">
-        @foreach ($threads as $thread)
-            <div class="card mt-3">
-              <div class="card-header">
-                <div class="level">
-                  <span class="flex">
-                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                  </span>
-
-                  <span>{{ $thread->created_at->diffForHumans() }}</span>
-                </div>
-              </div>
-              <div class="card-body">
-                <p>{{ $thread->body }}</p>
-              </div>
-            </div>
+        @foreach ($activities as $date => $activity)
+          <h3 class="page-header">{{ $date }}</h3>
+          @foreach ($activity as $record)
+            @include("profiles.activities.{$record->type}", ['activity' => $record])
+          @endforeach
         @endforeach
 
-        {{ $threads->links() }}
+        {{-- {{ $threads->links() }} --}}
       </div>
     </div>
   </div>
