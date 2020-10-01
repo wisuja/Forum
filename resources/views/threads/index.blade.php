67d8b11dc3ddd48c2622af1ce7b912a9e44a1bf8
wisuja <williamsj26.wsj@gmail.com>
2020-10-01 10:22:36 +0700

    Updating index view for threads

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,34 +4,27 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-					<div class="card mt-3">
-						<div class="card-header">
-							Forum Threads
-						</div>
-						<div class="card-body">
-							@if (session('status'))
-								<div class="alert alert-success" role="alert">
-									{{ session('status') }}
+					<h3>Forum Threads</h3>
+					@foreach ($threads as $thread)
+						<div class="card mt-3">
+							<div class="card-header">
+								<div class="level">
+									<h5 class="flex">
+										<a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+									</h5>
+									<a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}</a>
 								</div>
-							@endif
-
-         			@foreach ($threads as $thread)
-                <article>
-									<div class="level">
-										<h4 class="flex">
-											<a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-										</h4>
-
-										<a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}</a>
+							</div>
+							<div class="card-body">
+								@if (session('status'))
+									<div class="alert alert-success" role="alert">
+										{{ session('status') }}
 									</div>
-									<div class="body">
-										{{ $thread->body }}
-									</div>
-								</article>
-								<hr>
-								@endforeach
+								@endif
+								{{ $thread->body }}
+							</div>
 						</div>
-					</div>
+					@endforeach
       </div>
     </div>
 </div>
