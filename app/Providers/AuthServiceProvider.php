24bfbb064f23d55d35f80318c73c2e589edac561
wisuja <williamsj26.wsj@gmail.com>
2020-10-01 11:19:40 +0700

    Register ThreadPolicy and add admin role to user 'qwerty'

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,6 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
+        'App\Models\Thread' => 'App\Policies\ThreadPolicy'
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
@@ -25,6 +26,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::before(function ($user) {
+            if ($user->name == 'qwerty') {
+                return true;
+            }
+        });
     }
 }
