d8cf451ee1f812985cdbaafd70faabf1be2f4ed8
wisuja <williamsj26.wsj@gmail.com>
2020-10-01 10:55:10 +0700

    Handling DELETE route for deleting threads

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::get('/threads', [ThreadsController::class, 'index']);
 Route::get('/threads/create', [ThreadsController::class, 'create']);
 Route::get('/threads/{channel}', [ThreadsController::class, 'index']);
 Route::get('/threads/{channel}/{thread}', [ThreadsController::class, 'show']);
+Route::delete('/threads/{channel}/{thread}', [ThreadsController::class, 'destroy']);
 Route::post('/threads', [ThreadsController::class, 'store']);
 
 Route::post('/threads/{channel}/{thread}/replies', [RepliesController::class, 'store']);
