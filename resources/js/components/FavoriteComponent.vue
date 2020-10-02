d05836154bd5d71c58a1d3539ea5cd2491d50735
wisuja <williamsj26.wsj@gmail.com>
2020-10-02 15:30:18 +0700

    Creating a Vue component for favorite button

---
+++ resources/js/components/FavoriteComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <button type="submit" :class="classes" @click="toggle">
+    <i class="fas fa-heart"></i>
+    <span v-text="favoritesCount"></span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ["reply"],
+  data() {
+    return {
+      count: this.reply.favoritesCount,
+      isFavorited: this.reply.isFavorited,
+    };
+  },
+  computed: {
+    classes() {
+      return ["btn", this.isFavorited ? "btn-primary" : "btn-secondary"];
+    },
+    endpoint() {
+      return "/replies/" + this.reply.id + "/favorites";
+    },
+  },
+  methods: {
+    toggle() {
+      this.isFavorited ? this.unfavorite() : this.favorite();
+    },
+    favorite() {
+      axios.post(this.endpoint);
+      this.count++;
+      this.isFavorited = true;
+    },
+    unfavorite() {
+      axios.delete(this.endpoint);
+      this.count--;
+      this.isFavorited = false;
+    },
+  },
+};
+</script>
