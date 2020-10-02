a15e4260b7d6b46f1b93c41b5acb226d7534d472
wisuja <williamsj26.wsj@gmail.com>
2020-10-02 08:44:26 +0700

    Creating test for new feed method in Activity model

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use App\Models\Activity;
 use App\Models\Reply;
 use App\Models\Thread;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
@@ -37,5 +38,20 @@ class ActivityTest extends TestCase
         $reply = create(Reply::class);
 
         $this->assertEquals(2, Activity::count());
+    }
+
+    public function test_it_fetches_a_feed_for_any_user()
+    {
+        $this->signIn();
+
+        create(Thread::class, ['user_id' => auth()->id()], 2);
+
+        auth()->user()->activity()->first()->update(['created_at' => Carbon::now()->subWeek()]);
+
+        $feed = Activity::feed(auth()->user());
+
+        $this->assertTrue($feed->keys()->contains(Carbon::now()->format('Y-m-d')));
+
+        $this->assertTrue($feed->keys()->contains(Carbon::now()->subWeek()->format('Y-m-d')));
     }
 }
