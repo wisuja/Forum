950fa1f633e6bc3a8a0c044aeebb0a7dd1c99310
wisuja <williamsj26.wsj@gmail.com>
2020-10-02 11:26:03 +0700

    Registered ThreadPolicy in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Models\Thread' => 'App\Policies\ThreadPolicy'
+        'App\Models\Thread' => 'App\Policies\ThreadPolicy',
+        'App\Models\Reply' => 'App\Policies\ReplyPolicy'
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
@@ -27,7 +28,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::before(function ($user) {
-            if ($user->name == 'qwerty') {
+            if ($user->name == 'admin') {
                 return true;
             }
         });
