5651f309b9cf96211582f6e23e1d021cbc59dcea
wisuja <williamsj26.wsj@gmail.com>
2020-10-02 09:38:35 +0700

    Adding vue package to the project

---
+++ resources/js/components/FlashComponent.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="alert alert-success" role="alert">
+  A simple success alert—check it out!
+</div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
