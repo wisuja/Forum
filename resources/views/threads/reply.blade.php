d3cfd055cd93eeb977665eaadaad380ceb970e27
wisuja <williamsj26.wsj@gmail.com>
2020-10-02 15:30:02 +0700

    Refactoring with isolating favorite button to its own Vue component

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -6,12 +6,7 @@
           <a href="{{ route('profile', $reply->owner) }}">{{ $reply->owner->name }}</a> said
           {{ $reply->created_at->diffForHumans() }}
         </h5>
-        <form action="/replies/{{ $reply->id }}/favorites" method="POST">
-          @csrf
-          <button type="submit" class="btn btn-secondary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-            {{ $reply->favorites_count }} {{ Str::plural('favorite', $reply->favorites_count) }}
-          </button>
-        </form>
+        <favorite :reply="{{ $reply }}"></favorite>
       </div>
     </div>
     <div class="card-body">
