ca5f6152cc3f3115e45bf4e9104f186d672a1c4c
wisuja <williamsj26.wsj@gmail.com>
2020-10-02 11:26:55 +0700

    Creating a test for deleting a reply

--- tests/Feature/ParticipateInThreadTest.php
+++ tests/Feature/ParticipateInThreadTest.php
@@ -60,4 +60,28 @@ class ParticipateInThreadTest extends TestCase
         $this->post("{$thread->path()}/replies", $reply->toArray())
             ->assertSessionHasErrors('body');
     }
+
+    public function test_unauthorized_user_can_not_delete_a_reply()
+    {
+        $reply = create(Reply::class);
+
+        $this->withExceptionHandling()
+            ->delete('/replies/' . $reply->id)
+            ->assertRedirect(route('login'));
+
+        $this->signIn()
+            ->delete('/replies/' . $reply->id)
+            ->assertStatus(403);
+    }
+
+    public function test_authorized_user_can_delete_a_reply()
+    {
+        $this->signIn();
+        $reply = create(Reply::class, ['user_id' => auth()->id()]);
+
+        $this->delete('/replies/' . $reply->id)
+            ->assertStatus(302);
+
+        $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
+    }
 }
