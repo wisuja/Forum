65eec214639a7446928345536dd8db27e6a99830
wisuja <williamsj26.wsj@gmail.com>
2020-10-05 14:07:18 +0700

    Including ReplyFormComponent

--- resources/js/components/RepliesComponent.vue
+++ resources/js/components/RepliesComponent.vue
@@ -3,23 +3,31 @@
     <div v-for="(reply, index) in items" v-bind:key="reply.id">
       <reply :data="reply" @deleted="remove(index)"></reply>
     </div>
+    <reply-form class="mt-3" :endpoint="endpoint" @created="add"></reply-form>
   </div>
 </template>
   
 <script>
 import Reply from "./ReplyComponent.vue";
+import ReplyForm from "./ReplyFormComponent.vue";
 
 export default {
-  components: { Reply },
+  components: { Reply, ReplyForm },
   props: ["data"],
 
   data() {
     return {
       items: this.data,
+      endpoint: location.pathname + "/replies",
     };
   },
 
   methods: {
+    add(reply) {
+      this.items.push(reply);
+
+      this.$emit("created");
+    },
     remove(index) {
       this.items.splice(index, 1);
 
