26ff1b3acd6cddb4573a11a8df311a4776812542
wisuja <williamsj26.wsj@gmail.com>
2020-10-05 14:06:38 +0700

    Creating Vue component for Reply From

---
+++ resources/js/components/ReplyFormComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div v-if="signedIn">
+      <div class="form-group">
+        <textarea
+          name="body"
+          id="body"
+          class="form-control"
+          rows="5"
+          placeholder="Have something to say?"
+          v-model="body"
+          required
+        ></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary" @click="addReply">
+        Post
+      </button>
+    </div>
+    <div v-else class="text-center">
+      <p>
+        You need to <a href="/login">sign in</a> to participate in the thread.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["endpoint"],
+  data() {
+    return {
+      body: "",
+    };
+  },
+  computed: {
+    signedIn() {
+      return window.App.signedIn;
+    },
+  },
+  methods: {
+    addReply() {
+      axios.post(this.endpoint, { body: this.body }).then(({ data }) => {
+        this.body = "";
+
+        flash("Your reply has been posted.");
+
+        this.$emit("created", data);
+      });
+    },
+  },
+};
+</script>
