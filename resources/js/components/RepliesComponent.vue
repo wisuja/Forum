4d49a09f81a8c76631adebff6e0f9a64251171b8
wisuja <williamsj26.wsj@gmail.com>
2020-10-05 13:22:57 +0700

    Creating Replies Vue Component

---
+++ resources/js/components/RepliesComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-for="(reply, index) in items" v-bind:key="reply.id">
+      <reply :data="reply" @deleted="remove(index)"></reply>
+    </div>
+  </div>
+</template>
+  
+<script>
+import Reply from "./ReplyComponent.vue";
+
+export default {
+  components: { Reply },
+  props: ["data"],
+
+  data() {
+    return {
+      items: this.data,
+    };
+  },
+
+  methods: {
+    remove(index) {
+      this.items.splice(index, 1);
+
+      this.$emit("removed");
+
+      flash("Reply has been deleted.");
+    },
+  },
+};
+</script>
