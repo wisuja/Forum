0cd74e8e9ee02b26c48d64a8268fec69f03fa366
wisuja <williamsj26.wsj@gmail.com>
2020-10-05 10:20:10 +0700

    Fixing bug that happens when unfavorite a reply but still have favorite activity

--- app/Models/Favoritable.php
+++ app/Models/Favoritable.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 trait Favoritable
 {
+  public static function boot()
+  {
+    parent::boot();
+
+    static::deleting(function ($model) {
+      $model->favorites->each->delete();
+    });
+  }
+
   /**
    * A reply can be favorited.
    *
@@ -34,7 +43,7 @@ trait Favoritable
   {
     $attributes = ['user_id' => auth()->id()];
 
-    $this->favorites()->where($attributes)->delete();
+    $this->favorites()->where($attributes)->get()->each->delete();
   }
 
   /**
