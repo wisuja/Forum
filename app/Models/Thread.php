23824d0aa201858ba9ab1465820fe48da8890c79
wisuja <williamsj26.wsj@gmail.com>
2020-10-06 10:01:15 +0700

    Changing global scope to withCount method to get replies_count

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -13,13 +13,11 @@ class Thread extends Model
 
     protected $guarded = [];
 
+    protected $withCount = ['replies'];
+
     protected static function boot()
     {
         parent::boot();
-
-        static::addGlobalScope('replyCount', function ($builder) {
-            $builder->withCount('replies');
-        });
 
         static::deleting(function ($thread) {
             $thread->replies->each->delete();
