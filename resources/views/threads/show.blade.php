3a337304c8f3a603e8db2af61a81c4572050cff5
wisuja <williamsj26.wsj@gmail.com>
2020-10-06 11:37:19 +0700

    Adding SubcribeButton in Thread Vue Component

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -47,6 +47,10 @@
                             The thread was created on {{ $thread->created_at->diffForHumans() }} by
                             <a href="#">{{ $thread->creator->name }}</a>, and currently have <span v-text="repliesCount"></span> {{ Str::plural('comment', $thread->replies_count) }}.
                         </p>
+
+                        <p>
+                            <subscribe-button :active="{{ json_encode($thread->isSubscribedTo) }}"></subscribe-button>
+                        </p>
                     </div>
                 </div>
             </div>
