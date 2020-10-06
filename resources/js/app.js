a16898f4095d17e5151a226d15dc190eb3ae32a3
wisuja <williamsj26.wsj@gmail.com>
2020-10-06 11:37:26 +0700

    Reordering

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,9 +19,9 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+Vue.component('thread', require('./pages/ThreadComponent.vue').default);
 Vue.component('flash', require('./components/FlashComponent.vue').default);
 Vue.component('paginator', require('./components/PaginatorComponent.vue').default);
-Vue.component('thread', require('./pages/ThreadComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
