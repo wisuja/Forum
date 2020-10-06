4ea2364be5199d853aea56e95c7d9b38bffaf696
wisuja <williamsj26.wsj@gmail.com>
2020-10-06 09:30:07 +0700

    Removing endpoint variable

--- resources/js/components/ReplyFormComponent.vue
+++ resources/js/components/ReplyFormComponent.vue
@@ -26,7 +26,6 @@
 
 <script>
 export default {
-  props: ["endpoint"],
   data() {
     return {
       body: "",
@@ -39,13 +38,15 @@ export default {
   },
   methods: {
     addReply() {
-      axios.post(this.endpoint, { body: this.body }).then(({ data }) => {
-        this.body = "";
+      axios
+        .post(location.pathname + "/replies", { body: this.body })
+        .then(({ data }) => {
+          this.body = "";
 
-        flash("Your reply has been posted.");
+          flash("Your reply has been posted.");
 
-        this.$emit("created", data);
-      });
+          this.$emit("created", data);
+        });
     },
   },
 };
