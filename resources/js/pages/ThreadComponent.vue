97441f1fc4273ceb291425f5d09db97019706230
wisuja <williamsj26.wsj@gmail.com>
2020-10-06 11:37:05 +0700

    Referencing SubscribeButton

--- resources/js/pages/ThreadComponent.vue
+++ resources/js/pages/ThreadComponent.vue
@@ -1,8 +1,10 @@
 <script>
 import Replies from "../components/RepliesComponent.vue";
+import SubscribeButton from "../components/SubscribeButtonComponent.vue";
+
 export default {
   props: ["initialRepliesCount"],
-  components: { Replies },
+  components: { Replies, SubscribeButton },
   data() {
     return {
       repliesCount: this.initialRepliesCount,
