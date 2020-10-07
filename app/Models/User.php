3be973c8a5945067b11d530a76df7f8eb3ef0e27
wisuja <williamsj26.wsj@gmail.com>
2020-10-07 11:07:22 +0700

    Adding storing cache method in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -55,5 +56,15 @@ class User extends Authenticatable
     public function activity()
     {
         return $this->hasMany(Activity::class);
+    }
+
+    public function visitedThreadsCacheKey($thread)
+    {
+        return sprintf("user.%s.visits.%s", $this->id, $thread->id);
+    }
+
+    public function read($thread)
+    {
+        cache()->forever($this->visitedThreadsCacheKey($thread), Carbon::now());
     }
 }
