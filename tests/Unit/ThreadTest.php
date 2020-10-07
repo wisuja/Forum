22eea6ea410f154f95f6f401efffef936f6a8abc
wisuja <williamsj26.wsj@gmail.com>
2020-10-07 11:08:25 +0700

    Creating a test to check hasUpdatesFor function

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -6,6 +6,7 @@ use App\Models\Channel;
 use App\Models\Thread;
 use App\Models\User;
 use App\Notifications\ThreadWasUpdated;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Support\Facades\Notification;
@@ -129,5 +130,19 @@ class ThreadTest extends TestCase
         $thread->subscribe();
 
         $this->assertTrue($thread->isSubscribedTo);
+    }
+
+    public function test_a_thread_can_check_if_the_authenticated_user_has_read_all_replies()
+    {
+        $this->signIn();
+        $thread = create(Thread::class);
+
+        tap(auth()->user(), function ($user) use ($thread) {
+            $this->assertTrue($thread->hasUpdatesFor($user));
+
+            $user->read($thread);
+
+            $this->assertFalse($thread->hasUpdatesFor($user));
+        });
     }
 }
