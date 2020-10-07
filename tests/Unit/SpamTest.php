906b6eebe3358ee095f0b27cca6ea2d6872646f9
wisuja <williamsj26.wsj@gmail.com>
2020-10-07 13:41:23 +0700

    Adding a test for checking key being held down spam and refactoring the test

--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -2,15 +2,31 @@
 
 namespace Tests\Unit;
 
-use App\Models\Spam;
+use App\Inspections\Spam;
+use Exception;
 use Tests\TestCase;
 
 class SpamTest extends TestCase
 {
-    public function test_it_validates_spam()
+    public function test_it_checks_for_invalid_keywords()
     {
+        $this->withoutExceptionHandling();
+
         $spam = new Spam();
 
         $this->assertFalse($spam->detect('Innocent reply'));
+
+        $this->expectException(Exception::class);
+
+        $spam->detect('Yahoo Customer Support');
+    }
+
+    public function test_it_checks_for_any_key_being_held_down()
+    {
+        $spam = new Spam();
+
+        $this->expectException(Exception::class);
+
+        $spam->detect('Hello world aaaaaaaaaa');
     }
 }
