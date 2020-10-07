6b2b8da327e5e987fa9f8bbed78002f8a9e347c8
wisuja <williamsj26.wsj@gmail.com>
2020-10-07 09:56:25 +0700

    Refactoring addReply method

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ThreadHasNewReply;
 use App\Notifications\ThreadWasUpdated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -51,10 +52,7 @@ class Thread extends Model
     {
         $reply = $this->replies()->create($reply);
 
-        $this->subscriptions
-            ->filter(function ($subscription) use ($reply) {
-                return $subscription->user_id != $reply->user_id;
-            })->each->notify($reply);
+        $this->notifySubscribers($reply);
 
         return $reply;
     }
@@ -91,5 +89,13 @@ class Thread extends Model
         return $this->subscriptions()
             ->where('user_id', auth()->id())
             ->exists();
+    }
+
+    public function notifySubscribers($reply)
+    {
+        $this->subscriptions
+            ->where('user_id', '!=', $reply->user_id)
+            ->each
+            ->notify($reply);
     }
 }
