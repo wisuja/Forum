a61436aaec7d07f67b87210151feccb3d67aaf0f
wisuja <williamsj26.wsj@gmail.com>
2020-10-08 11:03:11 +0700

    Creating a test for lastReply method in User model

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Reply;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_a_user_can_fetch_their_most_recent_replies()
+    {
+        $user = create(User::class);
+
+        $reply = create(Reply::class, ['user_id' => $user->id]);
+
+        $this->assertEquals($reply->id, $user->lastReply->id);
+    }
+}
