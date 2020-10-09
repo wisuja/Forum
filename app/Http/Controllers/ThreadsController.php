b7e26abe170ff0da9a874dc79a873fd773995da8
wisuja <williamsj26.wsj@gmail.com>
2020-10-09 15:14:26 +0700

    Returning threads with pagination instead of threads data only

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -132,6 +132,6 @@ class ThreadsController extends Controller
             $threads->where('channel_id', $channel->id);
         }
 
-        return $threads->get();
+        return $threads->paginate(25);
     }
 }
