6170e9b0db1b4b57e0c1373737bc57df07ca723d
wisuja <williamsj26.wsj@gmail.com>
2020-10-09 15:14:05 +0700

    Enabling default style for paginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Channel;
 use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Validator;
@@ -30,6 +31,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrap();
+        
         View::composer('*', function ($view) {
             $channels = Cache::rememberForever('channels', function () {
                 return Channel::all();
