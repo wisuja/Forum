e665e65cb14e920b3d4914f4beb61f102b074f23
wisuja <williamsj26.wsj@gmail.com>
2020-10-09 10:15:42 +0700

    Accepting $reply from Thread model

---
+++ app/Events/ThreadReceivedNewReply.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ThreadReceivedNewReply
+{
+    use Dispatchable, SerializesModels;
+
+    public $reply;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($reply)
+    {
+        $this->reply = $reply;
+    }
+}
