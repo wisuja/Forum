66298517c1462bff78397262b606bf3795a34fbb
wisuja <williamsj26.wsj@gmail.com>
2020-10-12 10:19:26 +0700

    Creating a user test to generate a avatar_path

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -19,4 +19,14 @@ class UserTest extends TestCase
 
         $this->assertEquals($reply->id, $user->lastReply->id);
     }
+
+    public function test_a_user_can_determine_their_avatar_path() {
+        $user = create(User::class);
+
+        $this->assertEquals('avatars/default.jpg', $user->avatar());
+        
+        $user->avatar_path = 'avatars/me.jpg';
+
+        $this->assertEquals('avatars/me.jpg', $user->avatar());
+    }
 }
