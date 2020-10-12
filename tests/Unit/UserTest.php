f387b58f365585fd3576632b50ec3d8fe151e6f6
wisuja <williamsj26.wsj@gmail.com>
2020-10-12 12:24:10 +0700

    Fixing test errors

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -23,10 +23,10 @@ class UserTest extends TestCase
     public function test_a_user_can_determine_their_avatar_path() {
         $user = create(User::class);
 
-        $this->assertEquals('avatars/default.jpg', $user->avatar());
+        $this->assertEquals(asset('/storage/avatars/default.jpg'), $user->avatar_path);
         
         $user->avatar_path = 'avatars/me.jpg';
 
-        $this->assertEquals('avatars/me.jpg', $user->avatar());
+        $this->assertEquals(asset('/storage/avatars/me.jpg'), $user->avatar_path);
     }
 }
