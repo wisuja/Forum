ce5cd922dcdabc9182bc1e587088f8d46941dfa2
wisuja <williamsj26.wsj@gmail.com>
2020-10-12 09:23:51 +0700

    Adding store method in UserAvatarsController

---
+++ app/Http/Controllers/Api/UserAvatarsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserAvatarsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function store() 
+    {
+        request()->validate([
+            'avatar' => ['required', 'image']
+        ]);
+
+        auth()->user()->update([
+            'avatar_path' => request()->file('avatar')->store('avatars', 'public')
+        ]);
+    }
+}
