d2d60110925c0e9cf336b958c5b211bea1982dfc
wisuja <williamsj26.wsj@gmail.com>
2020-10-12 12:26:19 +0700

    Adding a section to displaying all trending threads

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -2,12 +2,28 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col-md-8">
             @include('threads._list')
 
             <div class="mt-3 text-center">
                 {{ $threads->links() }}
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Trending pages
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach ($trending as $thread)
+                            <li class="list-group-item">
+                                <a href="{{ url($thread->path) }}">{{ $thread->title }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
