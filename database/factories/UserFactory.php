13bdb94313847b0a753e8788aa4204c722620ce0
wisuja <williamsj26.wsj@gmail.com>
2020-10-13 12:36:14 +0700

    Adding state in UserFactory for assuming users always already confirmed their email

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,15 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'confirmed' => false
+            'confirmed' => true
         ];
+    }
+
+    public function unconfirmed() {
+        return $this->state(function($attributes) {
+            return [
+                'confirmed' => false
+            ];
+        });
     }
 }
