f66707c4f4b63adf8a594bebb92a52da5ecc61ac
wisuja <williamsj26.wsj@gmail.com>
2020-10-13 13:29:39 +0700

    Moving RegisterConfirmationsController to Auth folder instead of Api folder and refactoring code

--- app/Http/Controllers/Auth/RegisterConfirmationsController.php
+++ app/Http/Controllers/Auth/RegisterConfirmationsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
@@ -11,14 +11,11 @@ class RegisterConfirmationsController extends Controller
 {
     public function index() 
     {
-        try {
-            User::where('confirmation_token', request('token'))
-            ->firstOrFail()
-            ->confirm();
-        }catch (Exception $e) {
-            return redirect(route('threads'))
-                ->with('flash', 'Sorry, unknown token');
-        }
+        $user = User::where('confirmation_token', request('token'))->first();
+        
+        if (!$user) return redirect(route('threads'))->with('flash', 'Sorry, unknown token');
+        
+        $user->confirm();
 
         return redirect(route('threads'))
             ->with('flash', 'Your account is now confirmed!');
