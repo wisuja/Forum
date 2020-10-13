595b4b739b16484df8e420f3a9e96e6a6b459307
wisuja <williamsj26.wsj@gmail.com>
2020-10-13 09:22:40 +0700

    Applying middleware to check email confirmation on store method

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('/threads/create', [ThreadsController::class, 'create']);
 Route::get('/threads/{channel}', [ThreadsController::class, 'index']);
 Route::get('/threads/{channel}/{thread}', [ThreadsController::class, 'show']);
 Route::delete('/threads/{channel}/{thread}', [ThreadsController::class, 'destroy']);
-Route::post('/threads', [ThreadsController::class, 'store']);
+Route::post('/threads', [ThreadsController::class, 'store'])->middleware('must-be-confirmed');
 
 Route::get('/threads/{channel}/{thread}/replies', [RepliesController::class, 'index']);
 Route::post('/threads/{channel}/{thread}/replies', [RepliesController::class, 'store']);
