9609087b868bbd8786d596c6d52ad65e014c400b
wisuja <williamsj26.wsj@gmail.com>
2020-10-14 13:09:59 +0700

    Adding isBest attribute on Reply JSON data

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -16,7 +16,7 @@ class Reply extends Model
 
     protected $with = ['owner', 'favorites'];
 
-    protected $appends = ['favoritesCount', 'isFavorited'];
+    protected $appends = ['favoritesCount', 'isFavorited', 'isBest'];
 
     public function owner()
     {
@@ -51,5 +51,10 @@ class Reply extends Model
     public function isBest() 
     {
         return $this->thread->best_reply_id == $this->id;
+    }
+
+    public function getIsBestAttribute() 
+    {
+        return $this->isBest();
     }
 }
