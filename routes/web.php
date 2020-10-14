158dd460e73ad444d8517ddce28fe9938728bbb8
wisuja <williamsj26.wsj@gmail.com>
2020-10-14 15:38:45 +0700

    Changing default route to ThreadsController@index

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,7 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ThreadsController::class, 'index']);
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
