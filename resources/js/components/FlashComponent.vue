9e70d85dadcdc267314b4f89f8e2ac30cffc6536
wisuja <williamsj26.wsj@gmail.com>
2020-10-14 11:55:42 +0700

    Fix error in mistyping

--- resources/js/components/FlashComponent.vue
+++ resources/js/components/FlashComponent.vue
@@ -1,56 +1,56 @@
 <template>
-  <div
-    class="alert alert-flash"
-    :class="'alert-' + level"
-    role="alert"
-    v-show="show"
-    v-text="body"
-  ></div>
+    <div
+        class="alert alert-flash"
+        :class="'alert-' + level"
+        role="alert"
+        v-show="show"
+        v-text="body"
+    ></div>
 </template>
 
 <script>
 export default {
-  props: ["message"],
-  data() {
-    return {
-      body: this.message,
-      show: false,
-      level: "success",
-    };
-  },
+    props: ["message"],
+    data() {
+        return {
+            body: this.message,
+            show: false,
+            level: "success"
+        };
+    },
 
-  created() {
-    if (this.message) {
-      this.flash();
+    created() {
+        if (this.message) {
+            this.flash();
+        }
+
+        window.events.$on("flash", data => this.flash(data));
+    },
+
+    methods: {
+        flash(data) {
+            if (data) {
+                this.body = data.message;
+                this.level = data.level;
+            }
+            this.show = true;
+
+            this.hide();
+        },
+
+        hide() {
+            setTimeout(() => {
+                this.show = false;
+            }, 3000);
+        }
     }
-
-    window.events.$on("flash", (data) => this.flash(data));
-  },
-
-  methods: {
-    flash(data) {
-      if(data) {
-        this.body = message;
-        this.level = level;
-      }
-      this.show = true;
-
-      this.hide();
-    },
-
-    hide() {
-      setTimeout(() => {
-        this.show = false;
-      }, 3000);
-    },
-  },
 };
 </script>
 
 <style>
 .alert-flash {
-  position: fixed;
-  bottom: 25px;
-  right: 25px;
+    position: fixed;
+    bottom: 25px;
+    right: 25px;
 }
 </style>
