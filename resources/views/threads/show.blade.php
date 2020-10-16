c8e1041f0c4f00d7b27d2773b7888e0f27e5f0bf
wisuja <williamsj26.wsj@gmail.com>
2020-10-16 10:44:42 +0700

    Adding a button for toggling locked status in a thread

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<thread :initial-replies-count="{{ $thread->replies_count }}" inline-template>
+<thread :thread="{{ $thread}}" inline-template>
     <div class="container">
         <div class="row">
             <div class="col-md-8">
@@ -54,7 +54,8 @@
                         </p>
 
                         <p>
-                            <subscribe-button :active="{{ json_encode($thread->isSubscribedTo) }}"></subscribe-button>
+                            <subscribe-button :active="{{ json_encode($thread->isSubscribedTo) }}" v-if="signedIn"></subscribe-button>
+                            <button type="submit" class="btn btn-outline-danger" v-if="authorize('isAdmin')" @click="toggleLock" v-text="locked ? 'Unlock' : 'Lock'"></button>
                         </p>
                     </div>
                 </div>
