0eddb04b81d66e1c3806c3e0d8ea9e65b0128ee0
wisuja <williamsj26.wsj@gmail.com>
2020-10-16 10:42:38 +0700

    Removing unneccesary methods from Thread model

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -127,9 +127,4 @@ class Thread extends Model
     {
         $this->update(['best_reply_id' => $reply->id]);
     }
-
-    public function lock() 
-    {
-        $this->update(['locked' => true]);
-    }
 }
