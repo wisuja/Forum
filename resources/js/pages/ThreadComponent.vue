3a878205164c92e0ce191b261a0b0c7bf630ba27
wisuja <williamsj26.wsj@gmail.com>
2020-10-16 10:44:25 +0700

    Adding a method for toggling the locked status in Threads Vue

--- resources/js/pages/ThreadComponent.vue
+++ resources/js/pages/ThreadComponent.vue
@@ -3,12 +3,21 @@ import Replies from "../components/RepliesComponent.vue";
 import SubscribeButton from "../components/SubscribeButtonComponent.vue";
 
 export default {
-  props: ["initialRepliesCount"],
-  components: { Replies, SubscribeButton },
-  data() {
-    return {
-      repliesCount: this.initialRepliesCount,
-    };
-  },
+    props: ["thread"],
+    components: { Replies, SubscribeButton },
+    data() {
+        return {
+            repliesCount: this.thread.replies_count,
+            locked: this.thread.locked
+        };
+    },
+    methods: {
+        toggleLock() {
+            axios[this.locked ? "delete" : "post"](
+                `/locked-threads/${this.thread.slug}`
+            );
+            this.locked = !this.locked;
+        }
+    }
 };
 </script>
