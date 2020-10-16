b988cbeb2d8f65557f439b6554a4a27a0dca7b35
wisuja <williamsj26.wsj@gmail.com>
2020-10-16 09:47:16 +0700

    Checks if an user is administrator or not

--- app/Models/User.php
+++ app/Models/User.php
@@ -93,4 +93,9 @@ class User extends Authenticatable
         $this->confirmation_token = null;
         $this->save();
     }
+
+    public function isAdmin()
+    {
+        return in_array($this->email, ['admin@gmail.com']);
+    }
 }
