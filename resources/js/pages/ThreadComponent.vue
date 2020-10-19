76fdeb50cbb16e80e397c0dac06b5092d7a3df19
wisuja <williamsj26.wsj@gmail.com>
2020-10-19 15:22:08 +0700

    Adding update and cancel functionality in thread

--- resources/js/pages/ThreadComponent.vue
+++ resources/js/pages/ThreadComponent.vue
@@ -8,15 +8,44 @@ export default {
     data() {
         return {
             repliesCount: this.thread.replies_count,
-            locked: this.thread.locked
+            locked: this.thread.locked,
+            editing: false,
+            form: {
+                title: this.thread.title,
+                body: this.thread.body
+            }
         };
     },
     methods: {
         toggleLock() {
-            axios[this.locked ? "delete" : "post"](
-                `/locked-threads/${this.thread.slug}`
-            );
+            let uri = `/locked-threads/${this.thread.slug}`;
+            axios[this.locked ? "delete" : "post"](uri);
             this.locked = !this.locked;
+        },
+        update() {
+            let uri = `/threads/${this.thread.channel.slug}/${this.thread.slug}`;
+            axios
+                .patch(uri, this.form)
+                .then(() => {
+                    this.editing = false;
+                    flash("Your thread has been updated.");
+                })
+                .catch(error => {
+                    if (error.response.status == 422) {
+                        flash(
+                            "We encountered a problem when processing your requests. Please try again later.",
+                            "danger"
+                        );
+                    }
+                });
+        },
+        cancel() {
+            this.editing = false;
+
+            this.form = {
+                title: this.thread.title,
+                body: this.thread.body
+            };
         }
     }
 };
