a3c2fdb17bad0a696b497865d66c9179a700a6fd
wisuja <williamsj26.wsj@gmail.com>
2020-10-20 14:57:53 +0700

    Adding VueInstantSearch into Vue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,9 @@
 window._ = require("lodash");
 window.Vue = require("vue");
 
+import VueInstantSearch from "vue-instantsearch";
+Vue.use(VueInstantSearch);
+
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
