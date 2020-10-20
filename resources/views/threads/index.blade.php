d8354f9cdf0f0e0a6245ce879a74012a5e6e5eae
wisuja <williamsj26.wsj@gmail.com>
2020-10-20 11:40:23 +0700

    Adding a section for searching threads

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -11,6 +11,20 @@
             </div>
         </div>
         <div class="col-md-4">
+            <div class="card mb-3">
+                <div class="card-header">
+                    Search
+                </div>
+                <div class="card-body">
+                    <form action="/threads/search" method="GET">
+                        <div class="form-group">
+                            <input type="text" name="q" placeholder="Search..." class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Search</button>
+                    </form>
+                </div>
+            </div>
+
             @if (count($trending))
             <div class="card">
                 <div class="card-header">
