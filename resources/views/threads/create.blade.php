e15d188188cd1e9431662ce723521314aa59fb6c
wisuja <williamsj26.wsj@gmail.com>
2020-10-21 11:58:18 +0700

    Using trix editor instead of default textarea

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -40,7 +40,8 @@
                       </div>
                       <div class="form-group">
                         <label for="body">Body:</label>
-                        <textarea name="body" id="body" class="form-control" rows="5" required>{{ old('body') }}</textarea>
+                        <wysiwyg name="body"></wysiwyg>
+                        {{-- <textarea name="body" id="body" class="form-control" rows="5" required>{{ old('body') }}</textarea> --}}
                       </div>
                       <div class="form-group">
                         <div class="g-recaptcha" data-sitekey="{{ env('RECAPTCHA_SITEKEY') }}"></div>
