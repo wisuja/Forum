a3385296aa49b5dc4512677328d6330ee9d5efc4
wisuja <williamsj26.wsj@gmail.com>
2020-10-30 11:11:15 +0700

    Updating asset() to secure_asset()

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
     <script>
         window.App = {!! json_encode([
@@ -24,7 +24,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     <style>
         body {
             padding-bottom: 100px;
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('head')
-<link rel="stylesheet" href="{{ asset('css/vendor/jquery.atwho.css') }}">
+<link rel="stylesheet" href="{{ secure_asset('css/vendor/jquery.atwho.css') }}">
 @endsection
 
 @section('content')
