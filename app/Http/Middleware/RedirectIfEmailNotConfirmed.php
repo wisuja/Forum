6d194ae63e1b28b34f53608b5d959324379f2bf2
wisuja <williamsj26.wsj@gmail.com>
2020-11-06 11:20:40 +0700

    Adding response if it comes from AJAX request

--- app/Http/Middleware/RedirectIfEmailNotConfirmed.php
+++ app/Http/Middleware/RedirectIfEmailNotConfirmed.php
@@ -18,6 +18,10 @@ class RedirectIfEmailNotConfirmed
     {
         if(!$request->user()->confirmed) 
         {
+            if($request->expectsJson()) {
+                return response('You must first confirm your email address.', 403);
+            }
+            
             return redirect('/threads')->with('flash', 'You must first confirm your email address.');
         }
 
