e10a8990c61faf816fad482fcde4fd600929157d
wisuja <williamsj26.wsj@gmail.com>
2020-11-19 09:57:26 +0700

    Adding star button in AvatarForm

--- resources/js/components/AvatarFormComponent.vue
+++ resources/js/components/AvatarFormComponent.vue
@@ -1,53 +1,64 @@
 <template>
-  <div>
-    <div class="level">
-      <img :src="avatar" width="50" height="50" class="mr-1">
-      <h6 class="flex">
-        <h1 v-text="user.name"></h1>
-        <small v-text="joinedSince"></small>
-      </h6>
+    <div>
+        <div class="level">
+            <img :src="avatar" width="50" height="50" class="mr-1" />
+            <h6 class="flex">
+                <h1 v-text="user.name"></h1>
+                <small v-text="joinedSince"></small>
+            </h6>
+            <star-button
+                :data-stars="user.stars"
+                :name="user.name"
+            ></star-button>
+        </div>
+        <form v-if="canUpdate" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <image-upload
+                    name="avatar"
+                    @loaded="onLoad"
+                    class="form-control-file"
+                ></image-upload>
+            </div>
+        </form>
     </div>
-    <form v-if="canUpdate" method="POST" enctype="multipart/form-data">
-      <div class="form-group">
-        <image-upload name="avatar" @loaded="onLoad" class="form-control-file"></image-upload>
-      </div>
-    </form>
-  </div>
 </template>
 
 <script>
-import moment from 'moment';
-import ImageUpload from './ImageUploadComponent.vue';
+import moment from "moment";
+import ImageUpload from "./ImageUploadComponent.vue";
+import StarButton from "./StarButtonComponent.vue";
+
 export default {
-  props: ['user'],
-  components: { ImageUpload },
-  data() {
-    return {
-      avatar:  this.user.avatar_path
-    }
-  },
-  computed: {
-    canUpdate() {
-      return this.authorize(user => user.id == this.user.id);
+    props: ["user"],
+    components: { ImageUpload, StarButton },
+    data() {
+        return {
+            avatar: this.user.avatar_path
+        };
     },
-    joinedSince () {
-      return `Joined since ${moment(this.user.created_at).fromNow()}`;
-    }
-  },
-  methods: {
-    onLoad(avatar) {
-      this.avatar = avatar.src;
-
-      this.persist(avatar.file)
+    computed: {
+        canUpdate() {
+            return this.authorize(user => user.id == this.user.id);
+        },
+        joinedSince() {
+            return `Joined since ${moment(this.user.created_at).fromNow()}`;
+        }
     },
+    methods: {
+        onLoad(avatar) {
+            this.avatar = avatar.src;
 
-    persist(avatar) {
-      let data = new FormData();
-      data.append('avatar', avatar);
-      
-      axios.post(`/api/users/${this.user.name}/avatar`, data)
-        .then(flash('Avatar uploaded'));
+            this.persist(avatar.file);
+        },
+
+        persist(avatar) {
+            let data = new FormData();
+            data.append("avatar", avatar);
+
+            axios
+                .post(`/api/users/${this.user.name}/avatar`, data)
+                .then(flash("Avatar uploaded"));
+        }
     }
-  }
-}
+};
 </script>
